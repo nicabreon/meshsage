3379226367ad196c8d87c256aea7a2f91d9abedc
nicabreon <nicabreon@gmail.com>
2026-05-26 10:14:31 +0700

    feat(discovery): add DHT rendezvous peer discovery for global peers

--- pkg/network/discovery.go
+++ pkg/network/discovery.go
@@ -2,14 +2,20 @@ package network
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
+	"github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	"github.com/libp2p/go-libp2p/core/discovery"
+	"github.com/multiformats/go-multiaddr"
 	"github.com/nicabreon/meshsage/pkg/logger"
 )
 
-// DiscoveryServiceTag is the identifier for our application on the local network
+// DiscoveryServiceTag is the identifier for our application on the local and global network
 const DiscoveryServiceTag = "p2p-core-messaging"
 
 // mdnsNotifee gets notified when we find a new peer via mDNS
@@ -31,14 +37,97 @@ func (n *mdnsNotifee) HandlePeerFound(pi peer.AddrInfo) {
 	}
 }
 
-// SetupDiscovery sets up mDNS discovery to automatically find local peers
+// SetupDiscovery sets up mDNS discovery to automatically find local peers, and DHT routing discovery for global peers
 func SetupDiscovery(h host.Host) error {
-	// Register the notifee
+	// 1. Setup mDNS Service
 	n := &mdnsNotifee{h: h}
-	
-	// Create the mDNS service
 	s := mdns.NewMdnsService(h, DiscoveryServiceTag, n)
-	
-	// Start the service
-	return s.Start()
+	if err := s.Start(); err != nil {
+		logger.Error().Err(err).Msg("Failed to start mDNS service")
+	}
+
+	// 2. Setup DHT Rendezvous Discovery in the background
+	go func() {
+		// Wait for DHT bootstrapping to start
+		time.Sleep(5 * time.Second)
+		ctx := context.Background()
+
+		for {
+			// Don't advertise/discover if the network node is stopping
+			if h.Network().Peers() == nil {
+				return
+			}
+
+			if GlobalDHT != nil {
+				routingDisc := routing.NewRoutingDiscovery(GlobalDHT)
+				
+				// A. Advertise our presence on the DHT
+				logger.Debug().Msg("DHT Discovery: Advertising our presence to the swarm...")
+				routingDisc.Advertise(ctx, DiscoveryServiceTag, discovery.TTL(10*time.Minute))
+				
+				// B. Find other peers advertising on the DHT
+				peerChan, err := routingDisc.FindPeers(ctx, DiscoveryServiceTag, discovery.Limit(15))
+				if err == nil {
+					for pinfo := range peerChan {
+						if pinfo.ID == h.ID() {
+							continue
+						}
+						
+						// Connect if not already connected
+						if h.Network().Connectedness(pinfo.ID) != network.Connected {
+							logger.Info().Str("peerID", pinfo.ID.String()).Msg("DHT Discovery: Attempting connection to discovered peer")
+							
+							// Run dial in a separate goroutine so it doesn't block the channel reader
+							go func(pi peer.AddrInfo) {
+								dialCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+								defer cancel()
+								
+								// Proactively inject circuit relay addresses for the discovered target peer using all connected relays.
+								// This ensures libp2p can fall back to relay-routing if direct UDP connection has not punched through yet.
+								for _, p := range h.Network().Peers() {
+									protos, _ := h.Peerstore().GetProtocols(p)
+									isRelay := false
+									for _, proto := range protos {
+										if string(proto) == "/p2p-core/mailbox/1.0.0" {
+											isRelay = true
+											break
+										}
+									}
+									if isRelay {
+										// 1. Add short form
+										relayAddrStr := fmt.Sprintf("/p2p/%s/p2p-circuit/p2p/%s", p.String(), pi.ID.String())
+										ma, err := multiaddr.NewMultiaddr(relayAddrStr)
+										if err == nil {
+											h.Peerstore().AddAddr(pi.ID, ma, 5*time.Minute)
+										}
+										
+										// 2. Add full physical form
+										for _, addr := range h.Peerstore().Addrs(p) {
+											fullRelayAddrStr := fmt.Sprintf("%s/p2p/%s/p2p-circuit/p2p/%s", addr.String(), p.String(), pi.ID.String())
+											fullMa, err := multiaddr.NewMultiaddr(fullRelayAddrStr)
+											if err == nil {
+												h.Peerstore().AddAddr(pi.ID, fullMa, 5*time.Minute)
+											}
+										}
+									}
+								}
+
+								errDial := h.Connect(dialCtx, pi)
+								if errDial == nil {
+									logger.Info().Str("peerID", pi.ID.String()).Msg("DHT Discovery: Successfully connected")
+								} else {
+									logger.Debug().Err(errDial).Str("peerID", pi.ID.String()).Msg("DHT Discovery: Connection failed")
+								}
+							}(pinfo)
+						}
+					}
+				}
+			}
+			
+			// Poll peer discovery every 45 seconds
+			time.Sleep(45 * time.Second)
+		}
+	}()
+
+	return nil
 }
